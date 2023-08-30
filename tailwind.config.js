98c097846df00bd2dbe6382cb2d7a1c336ab1ccc
alain17-web <web2020.alain@gmail.com>
2023-08-30 23:03:28 +0200

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ['./*.html'],
   theme: {
     extend: {
-      screens:{
-        sm:"480px"
+      spacing:{
+        "big":"48rem"
       }
     },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
     fontFamily:{
-      nunito:['Nunito', 'sans-serif']
+      nunito:['Nunito','sans-serif']
     }
   },
   plugins: [],
